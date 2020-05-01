0be3a1c60c43fadc4cf57a325ea6b6b8b06384a3
Ievgen Melnyk <imelnyk@Ievgens-MBP.local>
2020-05-02 00:03:24 +0200

    first bad version simplify

--- FirstBadVersion/Solution.java
+++ FirstBadVersion/Solution.java
@@ -5,23 +5,15 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int low = 1;
         int high = n;
-        int mid = 1; // if n == 1 return 1
         while (low < high) {
             //mid = (low + high) / 2; // overflow!
-            mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
             if (isBadVersion(mid)) {
                 high = mid;
             } else {
-                low = mid;
-            }
-            if (high - low == 1) {
-                if (isBadVersion(low)) {
-                    return low;
-                } else {
-                    return high;
-                }
+                low = mid + 1;
             }
         }
-        return mid;
+        return low;
     }
 }
